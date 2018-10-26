9cee83395c5e5d1792f9e0e3e42c4d3c853016f2
nnovadmin <nnovadmin@NNOVADMIN>
2018-10-26 10:51:21 +0300

    Release

--- IvanPythonOS/IvanPythonOS.py
+++ IvanPythonOS/IvanPythonOS.py
@@ -1,35 +1,39 @@
 import random
-############################# Введём количество строк и столбцов
-rows=int(input('Num of rows: '))
-cols=int(input('Num of cols: '))
-############################# Сгенерируем массив заданного размера
-edge=int(input("Input treshold: "))
-r=random.randint(1,100)
+############################# пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+rows=int(input('Size of matrix: '))
+############################# пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+treshold=int(input("Input treshold: "))
+#r=random.randint(1,100)
 mas = []
+nec_mas=[]
 for i in range(rows):
     mas.append([])
-    for j in range(cols):
-        mas[i].append(r)
+    for j in range(rows):
         r = random.randint(1,100)  
+        mas[i].append(r)
+       
 sum=0
-masn=1 
-output=1
+masn=1
+output=0
+x=0
 print("Show us a randomized array")
 for i in range(rows):
     print(mas[i], "\n")
 print("Let's start to view necessary arrays")
 for i in range(rows):
-     for j in range(cols):
-        if(output==1): 
-            print("Array #",masn,": ", end='')
-        if(sum<edge or sum==edge):
+     nec_mas.append([])
+     for j in range(rows):
+        if(output==1):
+            print("Array #",nec_mas,": ", end='')
+        if(sum<treshold or sum==treshold):
+            nec_mas[x].append(mas[i][j])
             output=0
             sum=sum+mas[i][j]
-            print(mas[i][j],", ", end='' )
-        else: 
+            print(mas[i][j]," ", end='' )
+        else:
+            x=x+1
             print("\nSum: ", sum);
             print("====================")
-            masn=masn+1
+            #masn=masn+1
             sum=0
             output=1
- 
